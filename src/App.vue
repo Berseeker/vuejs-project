1c2ebed3cd18701241fb689313083530e1a2f4b3
Juan Gómez <juan.gomez@scalefast.com>
2023-03-09 16:20:22 +0100

    startup project vuejs

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive, ref, computed } from 'vue'
+
+const author = ref({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+// a computed ref
+const publishedBooksMessage = computed(() => {
+  return author.value.books.length > 0 ? 'Yes' : 'No'
+})
+</script>
+
+<template>
+  <p>Has published books:</p>
+  <span>{{ publishedBooksMessage }}</span>
+</template>
